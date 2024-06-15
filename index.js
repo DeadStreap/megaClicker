e9b876e6a9753821e4b9f7703981c16a14b4eb8a
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2024-06-15 22:37:23 +0300

    add clickPower text

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
 const clickCountOut = document.getElementById("clickCountOut")
+const clickPowerOut = document.getElementById("clickPowerOut")
 const clickBtn = document.getElementById("clickBtn")
 
 if(localStorage.getItem("userClickCount") == null ){
@@ -8,7 +9,15 @@ if(localStorage.getItem("userClickCount") == null ){
 }else{
     var userClickCount = Number(localStorage.getItem("userClickCount"));
 }
+if(localStorage.getItem("userClickPower") == null ){
+    localStorage.setItem("userClickPower", Number(1))
+    var userClickPower = Number(localStorage.getItem("userClickPower"));
+}else{
+    var userClickPower = Number(localStorage.getItem("userClickPower"));
+}
+
 renderUserClickCount(userClickCount);
+renderUserClickPower(userClickPower);
 
 clickBtn.onclick = () =>{
     userClickCount += 1;
@@ -16,11 +25,17 @@ clickBtn.onclick = () =>{
     renderUserClickCount(userClickCount);
 }
 
+
 function saveClickCount(userClickCount){
     localStorage.setItem("userClickCount", Number(userClickCount))
 }
+
 function renderUserClickCount(userClickCount){
     clickCountOut.innerHTML = userClickCount
+}
+
+function renderUserClickPower(userClickPower){
+    clickPowerOut.innerHTML = userClickPower
 }
 
 }
