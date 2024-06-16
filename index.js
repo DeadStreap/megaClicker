ea9850bcbc306d909abf92efcce1e135acd6d09e
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2024-06-16 14:53:33 +0300

    go back to work version

--- index.js
+++ index.js
@@ -22,16 +22,7 @@ if(localStorage.getItem("userInfo") == null ){
     localStorage.setItem("userInfo", JSON.stringify(user))
     var userInfo = JSON.parse(localStorage.getItem("userInfo"))
 }else{
-    const user = {
-        ClickCount: 0, 
-        ClickPower: 1,
-        ClickUpgradeNeededPoints: 1000,
-        AfkUpgradeNeededPoints: 1000,
-        AfkPower: 0,
-      };
-    localStorage.setItem("userInfo", JSON.stringify(user))
     var userInfo = JSON.parse(localStorage.getItem("userInfo"))
-   
 }   
 renderUserInfo(userInfo);
 
